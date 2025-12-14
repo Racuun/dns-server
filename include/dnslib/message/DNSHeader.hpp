dedbb5d2801fd7dbc106f4373b46ad12189da6b9
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2025-12-14 20:26:39 +0100

    migrate seter and geters in dnsheader to hpp

--- include/dnslib/message/DNSHeader.hpp
+++ include/dnslib/message/DNSHeader.hpp
@@ -41,17 +41,23 @@ namespace dnslib {
         static constexpr uint16_t CD_MASK     = 1 << 5;
         static constexpr uint16_t RCODE_MASK  = 0x000F;
 
-        void setResponse(bool r);
-        void setQuery(bool q);
-        void setAuthoritative(bool a);
-        void setTruncated(bool t);
-        void setRecursionDesired(bool rd);
-        void setRecursionAvailable(bool ra);
-        void setZ(bool z);
-        void setAuthenticData(bool ad);
-        void setCheckingDisabled(bool cd);
-        void setOpcode(OPCODE op);
-        void setRcode(RCODE rc);
+        void setResponse(bool r) { flags = r ? flags | QR_MASK : flags & ~QR_MASK; }
+        void setQuery(bool q) { flags = q ? flags & ~QR_MASK : flags | QR_MASK; }
+        void setAuthoritative(bool a) { flags = a ? flags | AA_MASK : flags & ~AA_MASK; }
+        void setTruncated(bool t) { flags = t ? flags | TC_MASK : flags & ~TC_MASK; }
+        void setRecursionDesired(bool rd) { flags = rd ? flags | RD_MASK : flags & ~RD_MASK; }
+        void setRecursionAvailable(bool ra) { flags = ra ? flags | RA_MASK : flags & ~RA_MASK; }
+        void setZ(bool z) { flags = z ? flags | 0x0070 : flags & ~0x0070; }
+        void setAuthenticData(bool ad) { flags = ad ? flags | AD_MASK : flags & ~AD_MASK; }
+        void setCheckingDisabled(bool cd) { flags = cd ? flags | CD_MASK : flags & ~CD_MASK; }
+        void setOpcode(OPCODE op) {
+            flags &= ~OPCODE_MASK;
+            flags |= (static_cast<uint16_t>(op) << 11) & OPCODE_MASK;
+        }
+        void setRcode(RCODE rc) {
+            flags &= ~RCODE_MASK;
+            flags |= static_cast<uint16_t>(rc) & RCODE_MASK;
+        }
 
     public:
         DNSHeader() = default;
@@ -59,17 +65,17 @@ namespace dnslib {
         void serialize(std::vector<uint8_t>& buff) const override;
         std::string toString() const override;
 
-        bool isRespone() const;
-        bool isQuery() const;
-        bool authAns() const;
-        bool truncation() const;
-        bool recursionDesired() const;
-        bool recursionAvailable() const;
-        bool z() const;
-        bool authenticData() const;
-        bool checkingDisabled() const;
-        OPCODE opcode() const;
-        RCODE rcode() const;
+        bool isRespone() const { return flags & QR_MASK; }
+        bool isQuery() const { return !isRespone(); }
+        bool authAns() const { return flags & AA_MASK; }
+        bool truncation() const { return flags & TC_MASK; }
+        bool recursionDesired() const { return flags & RD_MASK; }
+        bool recursionAvailable() const { return flags & RA_MASK; }
+        bool z() const { return flags & 0x0070; }
+        bool authenticData() const { return flags & AD_MASK; }
+        bool checkingDisabled() const { return flags & CD_MASK; }
+        OPCODE opcode() const { return static_cast<OPCODE>((flags >> 11) & OPCODE_MASK); }
+        RCODE rcode() const { return static_cast<RCODE>(flags & RCODE_MASK); }
     };
 
 }
--- src/dnslib/message/DNSHeader.cpp
+++ src/dnslib/message/DNSHeader.cpp
@@ -13,48 +13,15 @@ namespace dnslib {
         utils::writeU16(buff, anCount);
         utils::writeU16(buff, nsCount);
         utils::writeU16(buff, arCount);
-    }    std::string DNSHeader::toString() const {
-        
-
-    return "id: " + std::to_string(id) + 
+    }    
+    
+    std::string DNSHeader::toString() const {
+        return "id: " + std::to_string(id) + 
             "\nflags: " + std::to_string(flags) +
             "\nqdcount: " + std::to_string(qdCount) + 
             "\nancount: " + std::to_string(anCount) + 
             "\nnscount: " + std::to_string(nsCount) + 
             "\narcount: " + std::to_string(arCount);
     }
-
-    // Geters
-    bool DNSHeader::isRespone() const { return flags & QR_MASK; }
-    bool DNSHeader::isQuery() const { return !isRespone(); }
-    bool DNSHeader::authAns() const { return flags & AA_MASK; }
-    bool DNSHeader::truncation() const { return flags & TC_MASK; }
-    bool DNSHeader::recursionDesired() const { return flags & RD_MASK; }
-    bool DNSHeader::recursionAvailable() const { return flags & RA_MASK; }
-    bool DNSHeader::z() const { return flags & 0x0070; }
-    bool DNSHeader::authenticData() const { return flags & AD_MASK; }
-    bool DNSHeader::checkingDisabled() const { return flags & CD_MASK; }
-    OPCODE DNSHeader::opcode() const { return static_cast<OPCODE>((flags >> 11) & OPCODE_MASK); }
-    RCODE DNSHeader::rcode() const { return static_cast<RCODE>(flags & RCODE_MASK); }
-
-    // Seters
-    void DNSHeader::setResponse(bool r) { flags = r ? flags | QR_MASK : flags & ~QR_MASK; }
-    void DNSHeader::setQuery(bool q) { flags = q ? flags & ~QR_MASK : flags | QR_MASK; }
-    void DNSHeader::setAuthoritative(bool a) { flags = a ? flags | AA_MASK : flags & ~AA_MASK; }
-    void DNSHeader::setTruncated(bool t) { flags = t ? flags | TC_MASK : flags & ~TC_MASK; }
-    void DNSHeader::setRecursionDesired(bool rd) { flags = rd ? flags | RD_MASK : flags & ~RD_MASK; }
-    void DNSHeader::setRecursionAvailable(bool ra) { flags = ra ? flags | RA_MASK : flags & ~RA_MASK; }
-    void DNSHeader::setZ(bool z) { flags = z ? flags | 0x0070 : flags & ~0x0070; }
-    void DNSHeader::setAuthenticData(bool ad) { flags = ad ? flags | AD_MASK : flags & ~AD_MASK; }
-    void DNSHeader::setCheckingDisabled(bool cd) { flags = cd ? flags | CD_MASK : flags & ~CD_MASK; }
-    void DNSHeader::setOpcode(OPCODE op) {
-        flags &= ~OPCODE_MASK;
-        flags |= (static_cast<uint16_t>(op) << 11) & OPCODE_MASK;
-    }
-    void DNSHeader::setRcode(RCODE rc) {
-        flags &= ~RCODE_MASK;
-        flags |= static_cast<uint16_t>(rc) & RCODE_MASK;
-    }
-
 
 }
