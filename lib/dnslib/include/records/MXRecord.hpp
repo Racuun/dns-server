42165a62cf8ba570d2d0704578550475e0b0f8c8
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2026-01-13 22:40:26 +0100

    update documentation

--- lib/dnslib/include/records/MXRecord.hpp
+++ lib/dnslib/include/records/MXRecord.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file MXRecord.hpp
+ * @brief Defines the MXRecord class for representing a DNS 'MX' (Mail Exchange) record.
+ * @version 0.1
+ * @date 2026-01-01
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
 #pragma once
 
 #include "ResourceRecord.hpp"
@@ -6,20 +16,55 @@
 
 namespace dnslib {
 
+    /**
+     * @brief Represents a DNS 'MX' record, used to route email.
+     * 
+     * MX records specify the mail server responsible for accepting email messages
+     * on behalf of a recipient's domain, along with a preference value used to
+     * prioritize mail delivery if multiple servers are available.
+     */
     class MXRecord : public ResourceRecord {
     private:
         std::uint16_t preference;
         std::string exchange;
     public:
+        /**
+         * @brief Constructs a new MXRecord object.
+         * 
+         * @param n The domain name associated with the record.
+         * @param ttl The time-to-live for this record in seconds.
+         * @param pref The preference value (lower is higher priority).
+         * @param exch The domain name of the mail server.
+         */
         MXRecord(std::string n, uint32_t ttl, std::uint16_t pref, std::string exch)
             : ResourceRecord(n, 15, ttl), preference(pref), exchange(exch) {}
         
+        /**
+         * @brief Serializes the MXRecord into a byte buffer in DNS wire format.
+         * 
+         * @param buff The buffer to which the serialized data will be appended.
+         */
         void serialize(std::vector<uint8_t>& buff) const override;
 
+        /**
+         * @brief Returns a string representation of the MXRecord.
+         * 
+         * @return A std::string describing the MXRecord, typically "NAME TTL IN MX PREF EXCHANGE".
+         */
         std::string toString() const override;
 
+        /**
+         * @brief Gets the preference value of the mail server.
+         * 
+         * @return The 16-bit preference integer.
+         */
         std::uint16_t getPreference() const { return preference; }
         
+        /**
+         * @brief Gets the domain name of the mail server.
+         * 
+         * @return The exchange domain name.
+         */
         std::string getExchange() const { return exchange; }
     };
 
--- lib/dnslib/include/records/PTRRecord.hpp
+++ lib/dnslib/include/records/PTRRecord.hpp
@@ -1,20 +1,58 @@
+/**
+ * @file PTRRecord.hpp
+ * @brief Defines the PTRRecord class for representing a DNS 'PTR' (Pointer) record.
+ * @version 0.1
+ * @date 2026-01-01
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
 #pragma once
 
 #include "ResourceRecord.hpp"
 
 namespace dnslib {
 
+    /**
+     * @brief Represents a DNS 'PTR' record, primarily used for reverse DNS lookups.
+     * 
+     * PTR records map an IP address (in the form of a specially formatted domain name)
+     * back to a canonical domain name.
+     */
     class PTRRecord : public ResourceRecord {
     private:
         std::string ptr;
     public:
+        /**
+         * @brief Constructs a new PTRRecord object.
+         * 
+         * @param n The domain name representing the IP address (e.g., "1.0.168.192.in-addr.arpa").
+         * @param ttl The time-to-live for this record in seconds.
+         * @param ptr The canonical domain name associated with the IP.
+         */
         PTRRecord(std::string n, uint32_t ttl, std::string ptr)
             : ResourceRecord(n, 12, ttl), ptr(ptr) {}
 
+        /**
+         * @brief Serializes the PTRRecord into a byte buffer in DNS wire format.
+         * 
+         * @param buff The buffer to which the serialized data will be appended.
+         */
         void serialize(std::vector<uint8_t>& buff) const override;
 
+        /**
+         * @brief Returns a string representation of the PTRRecord.
+         * 
+         * @return A std::string describing the PTRRecord, typically "NAME TTL IN PTR DOMAIN".
+         */
         std::string toString() const override;
     
+        /**
+         * @brief Gets the domain name pointed to by this record.
+         * 
+         * @return The target domain name.
+         */
         std::string getPtr() const { return ptr; }
     
     };
