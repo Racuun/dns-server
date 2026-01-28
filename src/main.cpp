1ed774301d7f3f9ba74f30815da414bbe156c551
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2026-01-28 23:03:33 +0000

    enable daemon

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <cstdint>
 #include <fstream>
+#include <unistd.h>
+#include <string.h>
 
 #include "dns.hpp"
 
@@ -17,12 +19,27 @@
 #include "cache.hpp"
 
 
-int main(int, char**){
+int main(int argc, char** argv){
+    
+    bool runAsDaemon = false;
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--daemon") == 0) {
+            runAsDaemon = true;
+        }
+    }
+    
+    if (runAsDaemon) {
+        if (daemon(0, 0) == -1) {
+            std::cerr << "Failed to daemonize" << std::endl;
+            return 1;
+        }
+        utils::Logger::get().config(TARGET_SYSLOG("dns-server"), utils::LogLevel::INFO);
+    } else {
+        utils::Logger::get().config(TARGET_CONSOLE, utils::LogLevel::DEBUG);
+    }
+    
     // --- DNS START ---
-
-    utils::Logger::get().config(TARGET_CONSOLE, utils::LogLevel::DEBUG);
-
-    LOG_INFO("--- DNS Server Starting ---");
+    DNS_LOG_INFO("--- DNS Server Starting ---");
 
     TLRUCache dnsCache(1000);
 
@@ -31,14 +48,14 @@ int main(int, char**){
 
     /*
     std::thread logicThread(testThread, std::ref(qIn), std::ref(qOut));
-    LOG_INFO("Logic thread started");
+    DNS_LOG_INFO("Logic thread started");
     */
 
     std::thread logicThread(resolverWorker, std::ref(qIn), std::ref(qOut), std::ref(dnsCache));
-    LOG_INFO("Logic thread (Resolver) started");
+    DNS_LOG_INFO("Logic thread (Resolver) started");
 
     std::thread netThread(networkThread, 53, std::ref(qIn), std::ref(qOut));
-    LOG_INFO("Network thread started");
+    DNS_LOG_INFO("Network thread started");
 
     logicThread.join();
     netThread.join();
